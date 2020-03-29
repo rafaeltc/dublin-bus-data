2a9b106f72854015148cb1aa4715bfe6d392d213
Jose Carvalho <jose.carvalho-ext@man.eu>
2020-03-29 17:47:55 +0100

    add Verticle (Vertx)

--- src/main/kotlin/com/rafaeltc/dublinbusdata/DublinbusdataApplication.kt
+++ src/main/kotlin/com/rafaeltc/dublinbusdata/DublinbusdataApplication.kt
@@ -1,11 +1,23 @@
 package com.rafaeltc.dublinbusdata
 
+import com.rafaeltc.dublinbusdata.web.VertXServer
+import io.vertx.core.Vertx
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import javax.annotation.PostConstruct
+
 
 @SpringBootApplication
-class DublinbusdataApplication
+class DublinbusdataApplication(private val vertXServer: VertXServer) {
+	@PostConstruct
+	fun deployVerticle() {
+		Vertx.vertx().deployVerticle(vertXServer)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<DublinbusdataApplication>(*args)
 }
+
+
+
